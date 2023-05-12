ca96cd68150542c87e0cbd5b63f1604b9d390592
Mark Woodard <mark.c.woodard@gmail.com>
2023-05-12 16:55:16 -0500

    adding new repeater icons

---
+++ src/block-icons/repeater/variation-text-list/index.js
@@ -0,0 +1,27 @@
+export default <svg id="Layer_2" data-name="Layer 2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 252 166">
+  <g id="Layer_1-2" data-name="Layer 1">
+    <g>
+      <rect class="cls-3" fill="none" stroke="f0f0f0" stroke-miterlimit="10" stroke-width="2px" x="1" y="1" width="250" height="164" rx="5" ry="5"/>
+      <g>
+        <circle class="cls-2" fill="#616161" cx="32.25" cy="38" r="3"/>
+        <circle class="cls-2" fill="#616161" cx="32.25" cy="78" r="3"/>
+        <circle class="cls-2" fill="#616161" cx="32.25" cy="118" r="3"/>
+        <circle class="cls-2" fill="#616161" cx="136" cy="38" r="3"/>
+        <circle class="cls-2" fill="#616161" cx="136" cy="78" r="3"/>
+        <circle class="cls-2" fill="#616161" cx="136" cy="118" r="3"/>
+        <rect class="cls-1" fill="#939393" x="43.75" y="35" width="75" height="6" rx="3" ry="3"/>
+        <rect class="cls-1" fill="#939393" x="43.75" y="45" width="75" height="6" rx="3" ry="3"/>
+        <rect class="cls-1" fill="#939393" x="43.75" y="75" width="75" height="6" rx="3" ry="3"/>
+        <rect class="cls-1" fill="#939393" x="43.25" y="85" width="65" height="6" rx="3" ry="3"/>
+        <rect class="cls-1" fill="#939393" x="43.25" y="125" width="65" height="6" rx="3" ry="3"/>
+        <rect class="cls-1" fill="#939393" x="43.75" y="115" width="75" height="6" rx="3" ry="3"/>
+        <rect class="cls-1" fill="#939393" x="147.75" y="35" width="75" height="6" rx="3" ry="3"/>
+        <rect class="cls-1" fill="#939393" x="147.75" y="45" width="75" height="6" rx="3" ry="3"/>
+        <rect class="cls-1" fill="#939393" x="147.75" y="75" width="75" height="6" rx="3" ry="3"/>
+        <rect class="cls-1" fill="#939393" x="147.25" y="85" width="65" height="6" rx="3" ry="3"/>
+        <rect class="cls-1" fill="#939393" x="147.25" y="125" width="65" height="6" rx="3" ry="3"/>
+        <rect class="cls-1" fill="#939393" x="147.75" y="115" width="75" height="6" rx="3" ry="3"/>
+      </g>
+    </g>
+  </g>
+</svg>
