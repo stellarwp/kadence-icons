ed13afb147d574e4b5a413767beea3370410581f
Mark Woodard <mark.c.woodard@gmail.com>
2023-05-15 14:22:12 -0500

    icon fix

--- src/block-icons/repeater/variation-text-list/index.js
+++ src/block-icons/repeater/variation-text-list/index.js
@@ -3,24 +3,18 @@ export default <svg id="Layer_2" data-name="Layer 2" xmlns="http://www.w3.org/20
     <g>
       <rect class="cls-3" fill="none" stroke="f0f0f0" stroke-miterlimit="10" stroke-width="2px" x="1" y="1" width="250" height="164" rx="5" ry="5"/>
       <g>
-        <circle class="cls-2" fill="#616161" cx="32.25" cy="38" r="3"/>
-        <circle class="cls-2" fill="#616161" cx="32.25" cy="78" r="3"/>
-        <circle class="cls-2" fill="#616161" cx="32.25" cy="118" r="3"/>
-        <circle class="cls-2" fill="#616161" cx="136" cy="38" r="3"/>
-        <circle class="cls-2" fill="#616161" cx="136" cy="78" r="3"/>
-        <circle class="cls-2" fill="#616161" cx="136" cy="118" r="3"/>
-        <rect class="cls-1" fill="#939393" x="43.75" y="35" width="75" height="6" rx="3" ry="3"/>
-        <rect class="cls-1" fill="#939393" x="43.75" y="45" width="75" height="6" rx="3" ry="3"/>
-        <rect class="cls-1" fill="#939393" x="43.75" y="75" width="75" height="6" rx="3" ry="3"/>
-        <rect class="cls-1" fill="#939393" x="43.25" y="85" width="65" height="6" rx="3" ry="3"/>
-        <rect class="cls-1" fill="#939393" x="43.25" y="125" width="65" height="6" rx="3" ry="3"/>
-        <rect class="cls-1" fill="#939393" x="43.75" y="115" width="75" height="6" rx="3" ry="3"/>
-        <rect class="cls-1" fill="#939393" x="147.75" y="35" width="75" height="6" rx="3" ry="3"/>
-        <rect class="cls-1" fill="#939393" x="147.75" y="45" width="75" height="6" rx="3" ry="3"/>
-        <rect class="cls-1" fill="#939393" x="147.75" y="75" width="75" height="6" rx="3" ry="3"/>
-        <rect class="cls-1" fill="#939393" x="147.25" y="85" width="65" height="6" rx="3" ry="3"/>
-        <rect class="cls-1" fill="#939393" x="147.25" y="125" width="65" height="6" rx="3" ry="3"/>
-        <rect class="cls-1" fill="#939393" x="147.75" y="115" width="75" height="6" rx="3" ry="3"/>
+        <circle class="cls-2" fill="#616161" cx="41" cy="34" r="3"/>
+        <circle class="cls-2" fill="#616161" cx="41" cy="54" r="3"/>
+        <circle class="cls-2" fill="#616161" cx="41" cy="74" r="3"/>
+        <circle class="cls-2" fill="#616161" cx="41" cy="94" r="3"/>
+        <circle class="cls-2" fill="#616161" cx="41" cy="114" r="3"/>
+        <circle class="cls-2" fill="#616161" cx="41" cy="134" r="3"/>
+        <rect class="cls-1" fill="#939393" x="56" y="31" width="158" height="6" rx="3" ry="3"/>
+        <rect class="cls-1" fill="#939393" x="56" y="52" width="158" height="6" rx="3" ry="3"/>
+        <rect class="cls-1" fill="#939393" x="56" y="72" width="158" height="6" rx="3" ry="3"/>
+        <rect class="cls-1" fill="#939393" x="56" y="92" width="158" height="6" rx="3" ry="3"/>
+        <rect class="cls-1" fill="#939393" x="56" y="132" width="158" height="6" rx="3" ry="3"/>
+        <rect class="cls-1" fill="#939393" x="56" y="112" width="158" height="6" rx="3" ry="3"/>
       </g>
     </g>
   </g>
