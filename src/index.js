b456df4c69f4c1c7c5b43bc3e87dd8a2998ccb88
mark-c-woodard <49693677+mark-c-woodard@users.noreply.github.com>
2026-03-11 11:12:47 -0500

    Update index.js to include vector and pro icon

--- src/index.js
+++ src/index.js
@@ -289,7 +289,13 @@ export { default as lineMask } from './block-icons/progress-bar/line-mask-layout
 // Table Block
 export { default as tableBlockIcon } from './block-icons/table/table'
 export { default as tableRowBlockIcon } from './block-icons/table/table-row'
-export { default as tableDataBlockIcon } from './block-icons/table/table-data'
+export { default as tableDataBlockIcon } from './block-icons/table/table-data
+	
+// Vector
+export { default as VectorBlockIcon } from './block-icons/vector'
+
+// Pro
+export { default as proIcon } from './pro'
 
 // Arrows
 export { default as ArrowUp } from './arrow-up';
